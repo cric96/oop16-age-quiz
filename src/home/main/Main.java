38086097afa4ff22a7c0dcca7045d624bf4ae9bd
Gianluca <gianluca.aguzzi@studio.unibo.it>
2017-02-24 10:24:23 +0100

    Start project: setup the configuration file of checkstyle and pmd. add gitignore

---
+++ src/home/main/Main.java
@@ -0,0 +1,19 @@
+package home.main;
+
+/**
+ * This class is used to start the videogame
+ * and to set some values.
+ * 
+ */
+public final class Main {
+    private Main() { }
+    /** 
+     * @param args
+     *          not used in this contest 
+     */
+    public static void main(final String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
